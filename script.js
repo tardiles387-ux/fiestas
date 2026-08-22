ccfb7406107f61e08a7c5c18ef58341425b8c1a2
tardiles387-ux <tardiles387@gmail.com>
2026-08-22 14:41:23 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function abrirInvitacion() {
 }
 
 // CUENTA REGRESIVA (Configurada para el 22 de Agosto de 2026)
-const fechaCumple = new Date("Aug 22, 2026 21:00:00").getTime();
+const fechaCumple = new Date("Aug 22, 2026 21:30:00").getTime();
 
 const interval = setInterval(function() {
 	const ahora = new Date().getTime();
